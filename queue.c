10c7636074d8861baa4c4dc00da30531acbba67e
amutsamoses <amutsamoses@gmail.com>
2023-10-19 09:44:41 -0700

    Stack & Queue LIFO, FIFO

---
+++ queue.c
@@ -0,0 +1,56 @@
+#include "monty.h"
+
+/**
+ * f_queue - prints the top element of the Queue
+ *
+ * @head: head of the stack
+ *
+ * @counter: counts line_number
+ *
+ * Return: nothing
+*/
+
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - add node to the tail of the queue
+ *
+ * @n: the new_value to be added
+ * @head: head of the stack
+ *
+ * Return: Null
+*/
+
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
